0a0aeb230be268a5ebd0104e46bae9bcfa51c86f
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-09-27 17:19:52 +0200

    Declared and defined Calculator class;

--- include/calculator.h
+++ include/calculator.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Calculator
+{
+private:
+    // Contains history of the operations
+    std::unordered_map<std::string, int> history;
+    std::unordered_map<std::string, int (*)(int, int)> operationMap;
+    std::vector<std::string> operationStack;
+    std::unordered_map<std::string, int> resultMap;
+
+public:
+    void insertOperation(std::string &newOperation);
+    void printOperations();
+    void clearOperations();
+    void printHistory();
+    void clearHistory();
+    void printSolution();
+    virtual int solve() = 0;
+};
--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "include/calculator.h"
+
+void Calculator::insertOperation(std::string &newOperation)
+{
+    this->operationStack.push_back(newOperation);
+}
+
+void Calculator::printOperations()
+{
+    for (auto operation : this->operationStack)
+    {
+        std::cout << operation << std::endl;
+    }
+}
+
+void Calculator::clearOperations()
+{
+    this->operationStack.clear();
+}
+
+void Calculator::printSolution()
+{
+    for (auto [operation, result] : this->resultMap)
+    {
+        std::cout << operation << " = " << result;
+    }
+}
+
+void Calculator::printHistory()
+{
+    for (auto [operation, result] : this->history)
+    {
+        std::cout << operation << " = " << result << std::endl;
+    }
+}
+
+void Calculator::clearHistory()
+{
+    this->history.clear();
+}
