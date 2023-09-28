cd3676ef31d93772fab9776a5679a8f05f4fa04c
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-09-28 16:13:53 +0200

    Created RPN Calculator using calculator interface

--- include/RPN-Calculator.h
+++ include/RPN-Calculator.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "calculator.h"
+#include <vector>
+#include <string>
+
+class RPNCalculator : public Calculator
+{
+protected:
+    int solveOperation(const std::vector<std::string> &instructions);
+    std::vector<std::string> extractOperation(std::string &operationString);
+
+public:
+    RPNCalculator();
+    ~RPNCalculator();
+    void solve();
+};
--- src/RPN-calculator.cpp
+++ src/RPN-calculator.cpp
@@ -1,1 +1,73 @@
+#include "../include/calculator.h"
+#include "../include/RPN-Calculator.h"
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+
+int add(int a, int b) { return a + b; }
+int substract(int a, int b) { return b - a; }
+int multiply(int a, int b) { return a * b; }
+int divide(int a, int b) { return b / a; }
+
+RPNCalculator::RPNCalculator() : Calculator()
+{
+    operationMap = {{"+", &add},
+                    {"-", &substract},
+                    {"*", &multiply},
+                    {"/", &divide}};
+}
+
+RPNCalculator::~RPNCalculator(){};
+
+std::vector<std::string> RPNCalculator::extractOperation(std::string &operationString)
+{
+    std::vector<std::string> res;
+    std::string curr = "";
+    for (auto el : operationString)
+    {
+        if (el == ' ')
+        {
+            res.push_back(curr);
+            curr = "";
+        }
+        else
+        {
+            curr += el;
+        }
+    }
+    res.push_back(curr);
+    return res;
+}
+
+int RPNCalculator::solveOperation(const std::vector<std::string> &instructions)
+{
+    std::stack<int> valuesStack;
+    for (std::string instruction : instructions)
+    {
+        if (this->operationMap.find(instruction) == operationMap.end())
+        {
+            valuesStack.push(stoi(instruction));
+        }
+        else
+        {
+            int a = valuesStack.top();
+            valuesStack.pop();
+            int b = valuesStack.top();
+            valuesStack.pop();
+            valuesStack.push(this->operationMap[instruction](a, b));
+        }
+    }
+    return valuesStack.top();
+}
+
+void RPNCalculator::solve()
+{
+    int res;
+    for (std::string operationString : this->operationStack)
+    {
+        res = solveOperation(extractOperation(operationString));
+        this->resultMap[operationString] = res;
+        this->history[operationString] = res;
+    }
+}
